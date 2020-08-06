830d9fffde7f09c2aa57128655ee1d636b5be9a8
Jose Jorge <j.jorgem.s01@hotmail.com>
2020-08-05 21:26:23 -0500

    Se modifico boton, se agrego el titulo

--- Ejemplo/Form1.Designer.cs
+++ Ejemplo/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dtG_Alumono = new System.Windows.Forms.DataGridView();
             this.btn_NuevoAlumno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtG_Alumono)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,19 +45,30 @@
             // 
             // btn_NuevoAlumno
             // 
-            this.btn_NuevoAlumno.Location = new System.Drawing.Point(688, 8);
+            this.btn_NuevoAlumno.Location = new System.Drawing.Point(646, 3);
             this.btn_NuevoAlumno.Name = "btn_NuevoAlumno";
-            this.btn_NuevoAlumno.Size = new System.Drawing.Size(75, 23);
+            this.btn_NuevoAlumno.Size = new System.Drawing.Size(117, 28);
             this.btn_NuevoAlumno.TabIndex = 1;
             this.btn_NuevoAlumno.Text = "Nuevo Alumno";
             this.btn_NuevoAlumno.UseVisualStyleBackColor = true;
             this.btn_NuevoAlumno.Click += new System.EventHandler(this.btn_NuevoAlumno_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(348, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Alumnos.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_NuevoAlumno);
             this.Controls.Add(this.dtG_Alumono);
             this.Name = "Form1";
@@ -64,6 +76,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtG_Alumono)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +84,7 @@
 
         private System.Windows.Forms.DataGridView dtG_Alumono;
         private System.Windows.Forms.Button btn_NuevoAlumno;
+        private System.Windows.Forms.Label label1;
     }
 }
 
